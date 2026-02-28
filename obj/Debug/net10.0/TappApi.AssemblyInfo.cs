0d352580b4b8ba15d7786d81449bd940fc7e99a4
ekko <jeko.erg@gmail.com>
2026-02-28 21:54:22 +0400

    cc

--- obj/Debug/net10.0/TappApi.AssemblyInfo.cs
+++ obj/Debug/net10.0/TappApi.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("TappApi")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+35365b756c3e5a6aa8a6c9850bc9a6fdfa63a2fa")]
 [assembly: System.Reflection.AssemblyProductAttribute("TappApi")]
 [assembly: System.Reflection.AssemblyTitleAttribute("TappApi")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
